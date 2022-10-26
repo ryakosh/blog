a5537587fd95da6bb60b77e00eb3c391f15a2fdb
ryakosh <aryashokri@duck.com>
2022-10-26 15:29:59 +0330

    Convert WP website to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,13 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `ryakosh's blog`,
+    siteTitleAlt: `ryakosh's blog | Helping my future self and you`,
+    siteHeadline: `ryakosh's blog`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    siteUrl: `https://ryakosh.ir`,
+    siteDescription: `By creating this blog I intend to remind myself of what I learnt and did, maybe I’ll be able to help you too!.`, 
+    author: `@ryakosh`,
   },
   plugins: [
     {
@@ -32,17 +32,7 @@ module.exports = {
             title: `About`,
             slug: `/about`,
           },
-        ],
-        externalLinks: [
-          {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
-          },
-        ],
+        ], 
       },
     },
     {
@@ -54,9 +44,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `ryakosh's blog`,
+        short_name: `ryakosh's blog`,
+        description: `By creating this blog I intend to remind myself of what I learnt and did, maybe I’ll be able to help you too!.`,
         start_url: `/`,
         background_color: `#fff`,
         // This will impact how browsers show your PWA/website
@@ -121,7 +111,7 @@ module.exports = {
               }
             `,
             output: `rss.xml`,
-            title: `Minimal Blog - @lekoarts/gatsby-theme-minimal-blog`,
+            title: `ryakosh's blog`,
           },
         ],
       },
